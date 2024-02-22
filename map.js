a0340fd30f304c6f328991b90dcd692c727b5be0
shohag0007 <merinedrive007@gmail.com>
2024-02-22 20:31:21 +0600

    map function with awwow method

--- map.js
+++ map.js
@@ -1,15 +1,23 @@
-const numbers = [5, 6, 7, 8, 9, 10];
+
 // map ==> loops through each element of the array and do the operation that you passed in the call back function and hold the result from each operation in an array and finally returns you the array.
-function doubleIt(num){
-    return num * 3;
-}
-
-const result = numbers.map(doubleIt);
-console.log(result);
 
 
+const numbers = [5, 6, 7, 8, 9, 10];
+
+const double2 = numbers.map(n => n * 2);  // best arrow method
+
+// const result = numbers.map(double2);
 
 
+console.log(double2);
+
+// map ==> loops through each element of the array and do the operation that you passed in the call back function and hold the result from each operation in an array and finally returns you the array.
+// function doubleIt(num){
+//     console.log('Num Now', num);
+//     return num * 3;
+// }
+
+//console.log(result);
 
 // const doubled = [];
 // for( const num of numbers){
---
+++ more-map.js
@@ -0,0 +1,16 @@
+const numbers = [12, 8, 7, 20, 30 ];
+const doubled = numbers.map(x => x * 2);
+//console.log(doubled);
+
+const fiveAdded = numbers.map(x=> x + 5);
+//console.log(fiveAdded);
+
+const half = numbers.map(x => x /2);
+//console.log(half);
+
+const friends = [ 'tom', 'john', 'micheal', 'oliver'];
+const length = friends.map(fr => fr.length);
+//console.log(length);
+
+const firstLetter = friends.map(fr=>fr[0]);
+//console.log(firstLetter);
