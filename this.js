9f4917c8a6a4eb8d846e3d708f77917c476099dc
shohag0007 <merinedrive007@gmail.com>
2024-02-23 16:56:48 +0600

    this keyword

---
+++ this.js
@@ -0,0 +1,21 @@
+class Person {
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+
+    sleep (){
+        console.log(`Sleeping Now ${this.name}`)
+    }
+
+    activity (){
+        this.sleep();
+    }
+}
+
+const kodom = new Person('Parvez Masud', 25);
+console.log(kodom);
+//kodom.sleep();
+
+const vaja = new Person('vaja badam');
+vaja.sleep();
